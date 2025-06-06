6e1699b4b1ec3c60a47709a2d5360e4926ac7b5d
jackcollop <jackcollop@gmail.com>
2025-06-06 08:26:18 -0500

    Add files via upload

---
+++ lit3.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+r = pd.read_csv(r'rain.csv', header=None)
+
+r.set_index(0, inplace=True)
+
+r.columns= range(1980,2026)
+
+fig = px.funnel(r)
+
+st.plotly_chart(fig)
